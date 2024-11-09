0097a722c05af2c48fe2e37e26e49b598fdc4f37
Rene Alby <rene.alby@gmail.com>
2024-11-09 12:27:36 +0000

    minor updates

--- website/settings.py
+++ website/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 import os
+from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,10 +22,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-^1=bd&k037caz46a8_ta^rpv5m!qn3y@=w=(iuk%7j56ijs91a'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['18.132.109.205', 'renealby.com', '18.134.124.91', '127.0.0.1']
 
