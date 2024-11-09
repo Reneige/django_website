d7362bc7da48d5c2d13e87675b7c8adf398d168c
Rene Alby <rene.alby@gmail.com>
2024-11-09 13:10:02 +0000

    minor edits

--- website/settings.py
+++ website/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 
-if congig('DEBUG') == "True":
+if config('DEBUG') == "True":
 	DEBUG = True
 else:
 	DEBUG = False
